2360a4dea83f88339770d714eee90c5bc98a5f5a
Dilpreet <singhsince94@gmail.com>
2024-09-19 15:09:14 +0530

    basic guess game

--- Code/03 Gussing Game/src/main.rs
+++ Code/03 Gussing Game/src/main.rs
@@ -1,15 +1,47 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-
     println!("Guess the number!");
-    println!("Please enter the number");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut tries: i32 = 0;
 
-    let mut guess = String::new();
+    loop {
+        println!("Please enter the number");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // type casting using shadowing variable
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        // old pattern
+        // if (secret_number == guess) {
+        //     println!("You win!");
+        // } else {
+        //     println!("You lose!");
+        // }
+
+        // Rust pattern
+        // match picks the arm that satisfy the condition and executes it
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+        tries += 1;
+    }
+    println!("It took you {} tries to guess!", tries);
 }
